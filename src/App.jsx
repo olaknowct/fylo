599fb9dce131303e9eb28762a71ac891757f51ee
Christopher Olano <olanochristopher@gmail.com>
2023-04-16 23:58:31 +0800

    Refactor: create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
 import './App.css';
-import { ReactComponent as FyloLogo } from './images/logo-dark-mode.svg';
+import Header from './component/header/header.component';
 
 const App = () => {
   return (
     <div className='bg-darkMain text-whiteNeutral font-opensans'>
-      <header className='max-w-[144rem] flex flex-row items-center justify-between mx-auto px-32 py-[7.3rem]'>
-        <a href='#fylo'>
-          <FyloLogo />
-        </a>
-        <nav className=''>
-          <ul className='flex flex-row gap-[5.8rem] text-[1.6rem] items-center justify-center'>
-            <li>
-              <a href='#features'>Features</a>
-            </li>
-            <li>
-              <a href='#team'>Team</a>
-            </li>
-            <li>
-              <a href='#signin'>Sign in</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <Header />
     </div>
   );
 };
---
+++ src/component/header/header.component.jsx
@@ -0,0 +1,26 @@
+import { ReactComponent as FyloLogo } from '../../images/logo-dark-mode.svg';
+
+const Header = () => {
+  return (
+    <header className='max-w-[144rem] flex flex-row items-center justify-between mx-auto px-32 py-[7.3rem]'>
+      <a href='#fylo'>
+        <FyloLogo />
+      </a>
+      <nav className=''>
+        <ul className='flex flex-row gap-[5.8rem] text-[1.6rem] items-center justify-center'>
+          <li>
+            <a href='#features'>Features</a>
+          </li>
+          <li>
+            <a href='#team'>Team</a>
+          </li>
+          <li>
+            <a href='#signin'>Sign in</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
