6c1236462b8708bace8a395427c8520c22cf2cf8
Christopher Olano <olanochristopher@gmail.com>
2023-04-18 00:50:18 +0800

    grid footer

--- src/component/footer/footer.component.jsx
+++ src/component/footer/footer.component.jsx
@@ -9,19 +9,19 @@ import { ReactComponent as Email } from '../../images/icon-email.svg';
 const Footer = () => {
   return (
     <footer className='bg-darkBlue2'>
-      <div className=' py-[20rem] max-w-[120rem] mx-auto flex flex-col gap-14'>
-        <a href='#fylo'>
+      <div className=' py-[20rem] px-[2rem] max-w-[124rem] mx-auto flex flex-col gap-14'>
+        <a href='#fylo' className='md:mx-auto lg:mx-0'>
           <FyloLogo />
         </a>
-        <nav className='flex text-2xl flex-row justify-between'>
-          <p className=' gap-12 flex flex-row tracking-wide'>
-            <Location />
+        <nav className='grid grid-cols-6 text-2xl '>
+          <p className='gap-12 col-span-2 flex flex-row tracking-wide'>
+            <Location className='self-start flex' />
             <span className='max-w-lg'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
               incididunt ut labore et dolore magna aliqua
             </span>
           </p>
-          <ul className='flex flex-col gap-8'>
+          <ul className='flex col-span-1 ml-auto flex-col gap-8'>
             <li className='flex gap-6'>
               <Phone />
               +1-543-123-4567
@@ -31,18 +31,18 @@ const Footer = () => {
               example@fylo.com
             </li>
           </ul>
-          <ul className='flex flex-col gap-7'>
+          <ul className='flex ml-auto flex-col gap-7'>
             <li>About Us</li>
             <li>Jobs</li>
             <li>Press</li>
             <li>Blog</li>
           </ul>
-          <ul className='flex flex-col gap-7'>
+          <ul className='flex flex-col ml-auto gap-7'>
             <li>Contact Us</li>
             <li>Terms</li>
             <li>Privacy</li>
           </ul>
-          <ul className='flex items-center justify-center flex-row self-start  gap-6'>
+          <ul className='flex items-center justify-center flex-row ml-auto self-start gap-6'>
             <li className='p-1 rounded-full border'>
               <Fb />
             </li>
