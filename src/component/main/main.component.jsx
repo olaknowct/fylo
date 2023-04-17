33821d59c742e2b37d912c720c2442cf22850722
Christopher Olano <olanochristopher@gmail.com>
2023-04-17 13:55:36 +0800

    feature section completed

--- src/component/main/main.component.jsx
+++ src/component/main/main.component.jsx
@@ -1,13 +1,14 @@
-import { ReactComponent as CurvyDark } from '../../images/bg-curvy-dark-mode.svg';
 import { ReactComponent as AccessAnywhere } from '../../images/icon-access-anywhere.svg';
+import { ReactComponent as AnyFile } from '../../images/icon-any-file.svg';
+import { ReactComponent as Colab } from '../../images/icon-collaboration.svg';
 import { ReactComponent as Security } from '../../images/icon-security.svg';
 import IllustrationIntro from '../../images/illustration-intro.png';
 
 const Main = () => {
   return (
-    <main className='max-w-[144rem] mx-auto'>
-      <section className='pb-[16.5rem] '>
-        <figure className='relative z-0 flex flex-col items-center justify-center text-center font-bold gap-[3.8rem]'>
+    <main className=''>
+      <section className='pb-[16.5rem] bg-curvy-dark-mode bg-contain bg-no-repeat bg-bottom'>
+        <figure className='max-w-[144rem] mx-auto flex flex-col items-center justify-center text-center font-bold gap-[3.8rem]'>
           <img src={IllustrationIntro} alt='illustration intro' />
           <figcaption className='tracking-[0.01rem] flex flex-col justify-center items-center gap-[3.3rem]'>
             <h1 className='font-sans text-[4rem] max-w-6xl mx-auto '>
@@ -21,32 +22,55 @@ const Main = () => {
               Get Started
             </button>
           </figcaption>
-          <CurvyDark className='absolute -z-10 bottom-[-19rem]' />
         </figure>
       </section>
-      <section className='bg-darkBlue1 '>
-        <div className='max-w-[100rem] mx-auto justify-center grid grid-cols-2'>
-          <figure className='relative z-10 flex flex-col '>
+      <section className='bg-darkBlue1'>
+        <div className='max-w-[100rem] mx-auto justify-center grid grid-cols-2 text-center gap-y-32'>
+          <figure className='relative z-10 flex flex-col gap-9'>
             <div className='flex items-center justify-center h-[9rem]'>
               <AccessAnywhere className='' />
             </div>
-            <figcaption>
-              <h4>Access your file from anywhere</h4>
-              <p>
+            <figcaption className='max-w-xl mx-auto flex flex-col gap-4'>
+              <h4 className='text-[1.8rem] font-bold'>Access your files from anywhere</h4>
+              <p className='text-[1.4rem] font-thin'>
                 The ability to use a smartphone, tablet, or computer to access your account means
                 your files follow you everywhere.
               </p>
             </figcaption>
           </figure>
-          <figure className='relative z-10 flex flex-col '>
+          <figure className='relative z-10 flex flex-col gap-9'>
             <div className='flex items-center justify-center h-[9rem]'>
               <Security className='' />
             </div>
-            <figcaption>
-              <h4>Access your file from anywhere</h4>
-              <p>
-                The ability to use a smartphone, tablet, or computer to access your account means
-                your files follow you everywhere.
+            <figcaption className='max-w-xl mx-auto flex flex-col gap-4'>
+              <h4 className='text-[1.8rem] font-bold'>Security you can trust</h4>
+              <p className='text-[1.4rem] font-thin'>
+                2-factor authentication and user-controlled encryption are just a couple of the
+                security features we allow to help secure your files.
+              </p>
+            </figcaption>
+          </figure>
+          <figure className='relative z-10 flex flex-col gap-9'>
+            <div className='flex items-center justify-center h-[9rem]'>
+              <Colab className='' />
+            </div>
+            <figcaption className='max-w-xl mx-auto flex flex-col gap-4'>
+              <h4 className='text-[1.8rem] font-bold'>Real-time collaboration</h4>
+              <p className='text-[1.4rem] font-thin'>
+                Securely share files and folders with friends, family and colleagues for live
+                collaboration. No email attachments required
+              </p>
+            </figcaption>
+          </figure>
+          <figure className='relative z-10 flex flex-col gap-9'>
+            <div className='flex items-center justify-center h-[9rem]'>
+              <AnyFile className='' />
+            </div>
+            <figcaption className='max-w-xl mx-auto flex flex-col gap-4'>
+              <h4 className='text-[1.8rem] font-bold'>Store any type of file</h4>
+              <p className='text-[1.4rem] font-thin'>
+                Whether you're sharing holidays photos or work documents, Fylo has you covered
+                allowing for all file types to be securely stored and shared
               </p>
             </figcaption>
           </figure>
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,12 @@ module.exports = {
         sans: ['Raleway', 'sans-serif'],
         opensans: ['Open Sans', 'sans-serif'],
       },
+      backgroundImage: (theme) => ({
+        'logo-dark-mode': "url('images/logo-dark-mode.svg')",
+        'logo-light-mode': "url('images/logo-light-mode.svg')",
+        'curvy-dark-mode': "url('images/bg-curvy-dark-mode.svg')",
+        'curvy-light-mode': "url('images/bg-curvy-light-mode.svg')",
+      }),
     },
   },
   variants: {
